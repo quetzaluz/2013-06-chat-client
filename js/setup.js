84677cf600ebb2016f251b65210a08f76f72a6ec
quetzaluz <quetzaluz@gmail.com>
2013-07-01 11:39:22 -0700

    More data is stored on message nodes -- ID and created at.

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,49 @@
+$(document).ready(function () {
+  msgIntervalID = setInterval(function () {
+  	fetch();
+  }, 3000)();
+});
+
+var fetch = function () {
+	//makeMsg is called within this function. Get data from
+	//server and publish each message.
+	$.ajax('https://api.parse.com/1/classes/messages', {
+	  contentType: 'application/json',
+	  success: function(data){
+	  	console.log(data);
+	    for (var i = 0; i < data.results.length; i++) {
+	      makeMsg(data.results[i]);
+	    }
+	  },
+	  error: function(data) {
+	    console.log('Ajax request failed');
+	  }
+	});
+};
+
+var makeMsg = function (data) {
+	if (data.createdAt) {
+		var $msg = $('<span class="msg"></span><br>');
+		$msg.attr('data-created-at', data.createdAt);
+		$msg.attr('data-object-id', data.objectId);
+		$msg.text(data.text);
+		$msg.appendTo('#viewMsgs');
+	}
+}
+
+// var send = function (msgText) {
+// 	$.ajax({
+// 		type:"POST",
+// 		url: "https://api.parse.com/1/classes/chats",
+// 		data: JSON.stringify({text: ($.urlParam('username')+ ": " msg)})
+// 	});
+// };
+
+// $.urlParam = function(name){
+//   var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+//   return results[1] || 0;
+// }
+
 if(!/(&|\?)username=/.test(window.location.search)){
   var newSearch = window.location.search;
   if(newSearch !== '' & newSearch !== '?'){
@@ -26,9 +72,3 @@ $.ajax('https://api.parse.com/1/classes/messages', {
   }
 
 });
-
-var makeMsg = function (textData) {
-	var $msg = $('<span class="msg"></span>');
-	$msg.text(textData);
-	$msg.appendTo('#viewMsgs');
-}
