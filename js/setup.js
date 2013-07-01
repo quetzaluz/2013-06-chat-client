3c2e0c4ff10afaf44ad60fd2f8bea113b53466f6
quetzaluz <quetzaluz@gmail.com>
2013-07-01 10:45:06 -0700

    Basic retrieval of messages from server, but no updating.

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,23 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+$.ajax('https://api.parse.com/1/classes/messages', {
+  contentType: 'application/json',
+  success: function(data){
+    for (var i = 0; i < data.results.length; i++) {
+      makeMsg(data.results[i].text);
+    }
+    console.log(data);
+  },
+  error: function(data) {
+    console.log('Ajax request failed');
+  }
+
+});
+
+var makeMsg = function (textData) {
+	var $msg = $('<span class="msg"></span>');
+	$msg.text(textData);
+	$msg.appendTo('#viewMsgs');
+}
