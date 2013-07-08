7ef0751245d83837761e6bb8ef99cb90aef4b216
Hack Reactor Students <github@hackreactor.com>
2013-07-08 15:16:01 -0700

    Some modifications to make this work on a local server -- will continue local server version from new branch from now on.

--- js/setup.js
+++ js/setup.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
-	fetch();
-	$('#usernameField').val(getUsername());
+  fetch();
+  $('#usernameField').val(getUsername());
   msgIntervalID = setInterval(function () {
-  	fetch(lastTime);
+    fetch(lastTime);
   }, 3000);
 
   $('#usernameSubmit').on('click', function(e) {
-  	window.location.search = 'username=' + $('#usernameField').val();
+    window.location.search = 'username=' + $('#usernameField').val();
   });
 
   $('#textSubmit').on('click', function (e) {
-  	e.preventDefault();
+    e.preventDefault();
     msg = $('#msgField').val();
     send(msg);
-    $('#msgField').val('')
+    $('#msgField').val('');
   });
 
   $('#chatroomSubmit').on('click', function (e) {
-  	//should have a way to detect duplicate chatroom names
-  	e.preventDefault();
+    //should have a way to detect duplicate chatroom names
+    e.preventDefault();
     roomName = $('#chatroomField').val();
     var $room = $('<div class="chatroom"></div>');
     $room.text(roomName);
     $room.attr('id', roomName);
     $room.addClass('currentRoom');
     currentRoom = roomName;
-    $('.chatroom').each(function() {$(this).removeClass('currentRoom')});
-    $('#chatroomField').val('')
+    $('.chatroom').each(function() {$(this).removeClass('currentRoom');});
+    $('#chatroomField').val('');
     $room.appendTo('#rooms');
   });
 
   $(document).delegate('.chatroom', 'click', function () {
-  	console.log('clicked a room')
-  	$('.chatroom').each(function() {$(this).removeClass('currentRoom');});
-  	$(this).addClass('currentRoom');
-  	currentRoom = $(this).attr('id');
+    console.log('clicked a room');
+    $('.chatroom').each(function() {$(this).removeClass('currentRoom');});
+    $(this).addClass('currentRoom');
+    currentRoom = $(this).attr('id');
   });
 
   $(document).delegate('.usr', 'click', function () {
-  	if (friendList[$(this).attr('data-usr')]) {
-  		friendList[$(this).attr('data-usr')] = false;
-  	} else {
-  		friendList[$(this).attr('data-usr')] = true;
-  		$('.msg').each(function () {
+    if (friendList[$(this).attr('data-usr')]) {
+      friendList[$(this).attr('data-usr')] = false;
+    } else {
+      friendList[$(this).attr('data-usr')] = true;
+      $('.msg').each(function () {
         if (friendList[$(this).attr('data-usr')]) {
-  	      $(this).toggleClass('friend', true);
+          $(this).toggleClass('friend', true);
         }
       });
-  	}
+    }
   });
 
 });
@@ -58,68 +58,70 @@ var friendList = {};
 var currentRoom = 'default';
 
 var fetch = function (time) {
-	//record time of most recent message, and only publish most recent
-	//Date.parse('01/01/2011 10:20:45') > Date.parse('01/01/2011 5:10:10')
-	$.ajax('https://api.parse.com/1/classes/messages?order=-createdAt', {
-	  contentType: 'application/json',
-	  success: function(data){
-	  	//The code below repeats itself to an extent and can probably be
-	  	//refactored to eliminate repitition
-	  	if (!lastTime) {lastTime = data.results[99].createdAt}
-	  	if (currentRoom === 'default') {
-				for (var i = 99; i >0; i--) {
-					if (Date.parse(data.results[i].createdAt) > Date.parse(lastTime)) {
-						makeMsg(data.results[i]);
-					}
-				}	
-	  	} else {
-				for (var i = 99; i >0; i--) {
-					if (Date.parse(data.results[i].createdAt) > Date.parse(lastTime) && data.results[i].room === currentRoom) {
-						makeMsg(data.results[i]);
-					}
-				}	
+  //record time of most recent message, and only publish most recent
+  //Date.parse('01/01/2011 10:20:45') > Date.parse('01/01/2011 5:10:10')
+  $.ajax('http://127.0.0.1:8080/1/classes/messages', {
+    contentType: 'application/json',
+    type: "GET",
+    success: function(data){
+      //The code below repeats itself to an extent and can probably be
+      //refactored to eliminate repitition
+      console.log(data);
+      if (!lastTime) {lastTime = data.results[99].createdAt;}
+      if (currentRoom === 'default') {
+        for (var i = 99; i >0; i--) {
+          if (Date.parse(data.results[i].createdAt) > Date.parse(lastTime)) {
+            makeMsg(data.results[i]);
+          }
+        }
+      } else {
+        for (var i = 99; i >0; i--) {
+          if (Date.parse(data.results[i].createdAt) > Date.parse(lastTime) && data.results[i].room === currentRoom) {
+            makeMsg(data.results[i]);
+          }
+        }
 
-	  	}
-	  	
-	  },
+      }
 
-	  error: function(data) {
-	    console.log('Ajax request failed');
-	  }
-	});
+    },
+
+    error: function(data) {
+      console.log('Ajax request failed');
+    }
+  });
 };
 
 var makeMsg = function (data) {
-	if (data.objectId) {
-		var $msg = $('<span class="msg"></span>');
-		var $usr = $('<span class="usr"></span>')
-		$msg.text(data.text);
-		$usr.text(data.username);
-		$usr.attr('data-usr', data.username);
-		$msg.attr('data-usr', data.username);
-		if (friendList[data.username]) {
-    	$msg.addClass('friend');
+  if (data.objectId) {
+    var $msg = $('<span class="msg"></span>');
+    var $usr = $('<span class="usr"></span>');
+    $msg.text(data.text);
+    $usr.text(data.username);
+    $usr.attr('data-usr', data.username);
+    $msg.attr('data-usr', data.username);
+    if (friendList[data.username]) {
+      $msg.addClass('friend');
     }
-		$usr.prependTo($msg);
-		$msg.prependTo('#viewMsgs');
-		var lastTime = data.createdAt;
-	}
-}
+    $usr.prependTo($msg);
+    $msg.prependTo('#viewMsgs');
+    var lastTime = data.createdAt;
+  }
+};
 
 var getUsername = function(){
   var results = new RegExp('[\\?&]username=([^&#]*)').exec(window.location.href);
   return results[1] || 0;
-}
+};
 
 var send = function (msgText) {
-	console.log('sending...')
-	var usr = getUsername();
-	return $.ajax({
-		contentType: 'application/json',
-		type:"POST",
-		url: "https://api.parse.com/1/classes/messages",
-		data: JSON.stringify({text: msgText, username: usr, room: currentRoom})
-	});
+  console.log('sending...');
+  var usr = getUsername();
+  return $.ajax({
+    contentType: 'application/json',
+    type:"POST",
+    url: "http://127.0.0.1:8080/1/classes/messages",
+    data: JSON.stringify({text: msgText, username: usr, room: currentRoom})
+  });
 };
 
 if(!/(&|\?)username=/.test(window.location.search)){
